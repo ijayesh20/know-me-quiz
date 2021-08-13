5a58eb45d65bf68c8b8205da904da4f41cf6b2bc
ijayesh20 <76622083+ijayesh20@users.noreply.github.com>
2021-08-13 09:18:10 +0530

    added questions to ask
    questiions about me with answers

--- quiz.js
+++ quiz.js
@@ -5,3 +5,15 @@ readlineSync = require("readline-sync");
 var userName = readlineSync.question("What is your name?");
 console.log("Welcome " + userName +
   "! Let's start the game!");
+
+//array of objects 
+qna = [
+    {
+      question: "Where do you live?",
+      answer: "Mumbai"
+    },
+    {
+      question: "What is favourite hobby?",
+      answer: "Drawing"
+    }
+  ];
